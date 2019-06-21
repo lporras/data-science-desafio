b5edfc75e38ed7093de440be9889478299aa7e8b
Luis Porras <lporras16@gmail.com>
2019-06-20 21:58:31 -0400

    solucion letra O

--- ciclos/desafios/2102/letra_o.py
+++ ciclos/desafios/2102/letra_o.py
@@ -1,24 +1,8 @@
 def letra_o(n):
-  contain = ""
-
-  for i in range(n):
-    contain += "*"
-
-  contain += "\n"
-
-  contain_middle = "*"
-
-  for i in range(n - 2):
-    contain_middle += " "
-
-  contain_middle += "*\n"
-
-  contain += (contain_middle * (n - 2))[:-1] + "\n"
-
-  for i in range(n):
-    contain += "*"
-
-  return contain
+  base = "*" * n
+  middle = "*" + " " * (n - 2) + "*"
+  result = base + "\n" + (middle + "\n")*(n-2) + base
+  return result
 
 #print(letra_o(5))
 #print(letra_o(4))
