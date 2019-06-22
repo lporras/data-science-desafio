6315f2737fc9312a3ee415a9a7fd090c01dd45e2
Luis Porras <lporras16@gmail.com>
2019-06-22 12:37:49 -0400

    adding funciones demo

--- funciones/demo.py
+++ funciones/demo.py
@@ -2,3 +2,19 @@ def multiplicar():
     print(2 * 2)
 
 multiplicar()
+
+def multiplicar_dos_numeros(x, y):
+    print(x * y)
+
+multiplicar_dos_numeros(5, 2)
+
+a = int(input("Ingrese un numero:\n"))
+b = int(input("Ingrese un numero:\n"))
+
+multiplicar_dos_numeros(a, b)
+
+def multiplicar_opcional(x, y = 1):
+    print(x * y)
+
+multiplicar_opcional(10, 8)
+multiplicar_opcional(10)
