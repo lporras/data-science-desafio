7b863ce44536513c9b8fd09cd1d42235cf3c1a52
Luis Porras <lporras16@gmail.com>
2019-06-30 18:11:22 -0400

    api_rest folder

---
+++ api/api_y_rest/json.py
@@ -0,0 +1,3 @@
+import json
+
+diccionario = json.loads('{"clave": "valor"}')
---
+++ api/api_y_rest/request.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+payload = ""
+headers = {
+    'cache-control': "no-cache",
+    'Postman-Token': "3d304298-8c01-48d8-9a0d-5512f5a8ba9f"
+    }
+
+response = requests.request("GET", url, data=payload, headers=headers)
+
+print(response.text)
+print(response)
+result = json.loads(response.text)
+# In [19]: len(result)
+# Out[19]: 100
+
+# Codigos de respuesta
+# 1xx: Informacion
+# 2xx: Respuesta correcta
+# 3xx: Redirección
+# 4xx: Error del cliente
+# 5xx: Error del servidor
+
+# 401: Se requiere autenticacion
+# 200: todo bien
+# 403: No tiene permisos de acceso
+# 404: no encontrado
